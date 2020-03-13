a611483069aaf153a100d1dff377cb3a20d48525
Joshua Christensen <joshua@thedigitaldesign.com>
2020-03-12 23:39:09 -0600

    updated packages and am continuing to build out the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+import { Button } from "react-bootstrap";
 
 class App extends Component {
-    state = {
-        gifts: []
-    }
+  state = {
+    gifts: []
+  };
 
-    render() {
-        return (
-            <main>
-                <h2>Gift Giver</h2>
-                <h3>Birthday List</h3>
-            </main>
-        )
-    }
+  addGift = () => {
+    const { gifts } = this.state;
+    const ids = this.state.gifts.map(gift => gift.id);
+    const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+    gifts.push({
+      id: maxId + 1
+    });
+
+    this.setState({ gifts });
+  };
+
+  render() {
+    return (
+      <main>
+        <h2>Gift Giver</h2>
+        <h3>Birthday List</h3>
+
+        <Button className="button-add" onClick={this.addGift}>
+          Add Gift
+        </Button>
+      </main>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 // Packages
-import { shallow } from 'enzyme'
+import { shallow } from "enzyme";
 
 // Components
-import App from './App'
+import App from "./App";
 
+const app = shallow(<App />);
 
-const app = shallow(<App />)
+it("renders correctly", () => {
+  expect(app).toMatchSnapshot();
+});
 
-it('renders correctly', () => {
-    expect(app).toMatchSnapshot()
-})
+it("initializes the `state` with an empty list of gifts", () => {
+  expect(app.state().gifts).toEqual([]);
+});
 
-it('initializes the `state` with an empty list of gifts', () => {
-    expect(app.state().gifts).toEqual([])
-})
+it("adds a new gift to `state` when clicking the `add gift` button", () => {
+  app.find(".button-add").simulate("click");
+
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
